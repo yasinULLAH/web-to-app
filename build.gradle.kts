4c7608a38ccd625230a97ace78f25b71fd10fedc
yasinULLAH <115789093+yasinULLAH@users.noreply.github.com>
2026-05-18 15:53:40 +0500

    Fix Kotlin/KSP plugin versions (2.2.10 and 2.3.2 dont exist; use 2.1.21 stable)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 
 plugins {
     id("com.android.application") version "9.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("com.google.devtools.ksp") version "2.3.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("com.google.devtools.ksp") version "2.1.21-1.0.31" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
